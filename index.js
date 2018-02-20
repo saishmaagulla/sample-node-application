f214c623e5d36172d684b4a650ef0b03977a881f
rohith-vallabhaneni <rohithchowdaryvallabhaneni@gmail.com>
2018-02-20 23:37:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const server = require('./provider.js').server
 const PORT = process.env.PORT || 30000
+const ADDRESS = process.env.ADDRESS || "0.0.0.0"
 
-server.listen(PORT, () => {
-  console.log('Provider Service listening on http://localhost:'+ PORT)
+
+server.listen(PORT, ADDRESS, () => {
+  console.log('Provider Service listening on http://'+ADDRESS+':'+ PORT)
 })
